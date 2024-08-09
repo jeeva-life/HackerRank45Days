38d8a6634eb28f895eac888b0cf76713626db4f9
jeeva-life <kodalijeevan756@gmail.com>
2024-08-09 14:04:38 -0400

    finding subSequence oF HACKERRANK is present in that order or not

---
+++ subSequence.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'hackerrankInString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String hackerrankInString(String s) {
+    // Write your code here
+        Stack<Character> sh = new Stack<>();
+        String st = "hackerrank";
+        List<Character> ss = st.chars().mapToObj(c -> (char)c).collect(Collectors.toList());
+        for(int i=ss.size()-1;i>=0;i--){
+            sh.push(ss.get(i));
+        }
+        
+        List<Character> l = s.chars().mapToObj(c -> (char)c).collect(Collectors.toList());
+        for(int i=0;i<l.size();i++){
+            if(l.get(i)==sh.peek() && sh.size()>0){
+                    sh.pop();
+                }
+            if(sh.size()==0) return "YES";
+            
+        }
+        if(sh.size()==0){
+            return "YES";
+        }
+        return "NO";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.hackerrankInString(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
